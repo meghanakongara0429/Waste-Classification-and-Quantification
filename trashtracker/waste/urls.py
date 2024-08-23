de2395d47e086bf392c786befd2a658e6164856e
Meghana Kongara <154802977+meghanakongara0429@users.noreply.github.com>
2024-08-23 18:48:44 +0530

    Delete trashtracker directory

--- trashtracker/waste/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload/', views.upload, name='upload'),
-    path('results/', views.results, name='results'),
-]
